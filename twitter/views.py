1c9361b6254d5e6a2303f067abfa9613797f4dd0
meernahro <meernahro11@gmail.com>
2023-01-22 12:55:56 +0300

    make further changes to index and add a new function to views.py to fetch data

--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
     return render(request,"twitter/index.html")
+
+def actions(request):
+
+    # Get start and end points
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or (start + 9))
+
+    # Generate list of arbitrary data and pack it up and send it
+    def generateData():
+        return(["wertgvc","wertg","qazxw","poiuiolk"])
+
+    data = []
+    for i in range(start, end+1):
+        data.append(generateData())
+
+    return JsonResponse({
+        "actions":data
+    })
+
+
